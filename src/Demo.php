ea5e843ed057c8465b7e1cfaa30d4897686064ad
verwirrt <2432775177@qq.com>
2023-05-30 18:02:27 +0800

    1

---
+++ src/Demo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Jutuike;
+
+
+/**
+ * Class Demo
+ * @package Xuchen\Biubiubiu
+ */
+class Demo
+{
+    public static function execute()
+    {
+        echo 'It works!';
+    }
+}
+
